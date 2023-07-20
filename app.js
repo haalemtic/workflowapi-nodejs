a11a6555d839db07210ece58eb0633b977feeb5d
haalemtic <haalemtic@gmail.com>
2023-07-20 08:46:53 +0000

    autorize cors

--- app.js
+++ app.js
@@ -18,6 +18,17 @@ const path = require('path');
 const app = express();
 
 
+
+// permettre l'accès à l'API (CORS)
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token,Origin, X-Requested-With, Content, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 app.use(bodyParser.json());
 app.get('/', (req, res) => {
   const filePath = path.join(__dirname, 'index.html');
